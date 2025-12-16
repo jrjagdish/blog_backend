9326cd33089ce3acf0d8a8cb74b16c9a5b2257cb
jrjagdish <hanamshettyjagdish1@gmail.com>
2025-12-16 15:12:47 +0530

    first version is complete

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -31,35 +31,45 @@ export const createPost = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+export const getPosts = async (req, res) => {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+    const posts = await Post.find().sort({ published: -1 }).skip(parseInt(skip)).limit(parseInt(limit)).populate("authorId", "username");
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 export const updatePosts = async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (!post) {
-            return res.status(404).json({ error: "Post not found." });
-        }
-        if (post.authorId.toString() !== req.user.userId) {
-            return res.status(403).json({ error: "Unauthorized." });
-        }
-        const { title, content, featuredImage, tags } = req.body;
-        if(content) post.content = content;
-        if(featuredImage) post.featuredImage = featuredImage;
-        if(tags) post.tags = tags;
-        await post.save();
-        res.json(post);
-    }catch(err){
-        res.status(500).json({ error: err.message });
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found." });
     }
-}
+    if (post.authorId.toString() !== req.user.userId) {
+      return res.status(403).json({ error: "Unauthorized." });
+    }
+    const { title, content, featuredImage, tags } = req.body;
+    if (content) post.content = content;
+    if (featuredImage) post.featuredImage = featuredImage;
+    if (tags) post.tags = tags;
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 export const deletePost = async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (!post) return res.status(404).json({ error: "Post not found." });
-        if (post.authorId.toString() !== req.user.userId) {
-            return res.status(403).json({ error: "Unauthorized." });
-        }
-        await post.deleteOne();
-        res.json({ message: "Post deleted successfully." });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ error: "Post not found." });
+    if (post.authorId.toString() !== req.user.userId) {
+      return res.status(403).json({ error: "Unauthorized." });
     }
-}
+    await post.deleteOne();
+    res.json({ message: "Post deleted successfully." });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost,updatePosts,deletePost } from "../controllers/postController.js";
+import { createPost,updatePosts,deletePost ,getPosts} from "../controllers/postController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createPost);
+router.get("/", protect, getPosts);
 router.get("/ping", (req, res) => {
   res.send("Posts router working");
 });
